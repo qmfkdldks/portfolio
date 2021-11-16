04fe4e2f2f70bda4439e657162d6e812df53532a
Brian Kang <qmfkdldks@gmail.com>
2021-11-16 15:06:02 -0300

    post page update

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -9,6 +9,20 @@ import AppMarkdown from "../../library/withParallax/README.md";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { xonokai } from "react-syntax-highlighter/dist/esm/styles/prism";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-family: "Raleway", sans-serif;
+  font-size: 2.25rem;
+  line-height: 1.05;
+`;
 
 export default function PostPage() {
   const [markdown, setSource] = React.useState("");
@@ -20,37 +34,34 @@ export default function PostPage() {
   }, []);
 
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        transition: "all 0.5s ease",
-      }}
-    >
-      <h1>hihihi</h1>
-      <ReactMarkdown
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || "");
-            return !inline && match ? (
-              <SyntaxHighlighter
-                children={String(children).replace(/\n$/, "")}
-                style={xonokai}
-                language={match[1]}
-                PreTag="div"
-                {...props}
-              />
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      >
-        {markdown}
-      </ReactMarkdown>
-    </div>
+    <Container>
+      <Title>Higher Order Component Parallex Effect</Title>
+
+      <div>
+        <ReactMarkdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, "")}
+                  style={xonokai}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {markdown}
+        </ReactMarkdown>
+      </div>
+    </Container>
   );
 }
 
